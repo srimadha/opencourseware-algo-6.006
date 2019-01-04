8e5b4088fb1a9e972d190c861a1d3fb3a40ed89b
Srikanth Madhava <srikanth.attur@gmail.com>
2019-01-04 12:58:35 -0500

    Generics for quicksort

--- algos/src/com/mit/opencourseware/algo/lecture3/QuickSort.scala
+++ algos/src/com/mit/opencourseware/algo/lecture3/QuickSort.scala
@@ -1,25 +1,28 @@
 object QuickSort{
 
-  def quickSort( arr : Array[Int] ) : Array[Int] = {
+  def quickSort[T]( arr : List[T], func: ( T, T) => Int ) : List[T] = {
 
     if( arr.length <= 1)  arr
-
     else {
       val pivot = arr(arr.length / 2)
-
-      Array.concat(
-        quickSort(arr filter (pivot >)),
-        arr filter (pivot ==),
-        quickSort(arr filter (pivot <))
+      List.concat(
+        quickSort(arr filter (func(_, pivot) == -1), func),
+        arr filter (func(_, pivot) == 0),
+        quickSort(arr filter (func(_, pivot) == 1), func)
       )
     }
   }
 
+  def func( a : Int, b: Int) : Int = {
+    if( a == b) return 0
+    else if(a < b) return -1
+    else return 1
+  }
+
   def main( args : Array[String]) {
     val random = scala.util.Random
-
-    val arr = random.shuffle( 1 to 80) toArray
-      val solved: Array[Int] = quickSort(arr)
+    val arr = random.shuffle( 1 to 80) toList
+    val solved: List[Int] = quickSort(arr, func)
 
     for( i <- 0 to arr.length - 1){
       print( arr(i) + ",");

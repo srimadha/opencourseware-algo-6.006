a42a72d0d7ebc6b40a0dea57f9aa7323129e0a0e
srikanthmadhava <srikanth.attur@gmail.com>
2020-01-24 08:45:11 -0500

    update

--- algos/src/com/leetcode/hard/AlienOrder.java
+++ algos/src/com/leetcode/hard/AlienOrder.java
@@ -4,8 +4,56 @@ import java.util.*;
 
 /**
  * Created by Sri on 5/11/2019.
+ *
+ * https://leetcode.com/problems/alien-dictionary/
+ *
+ * There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. You receive a list of non-empty words from the dictionary, where words are sorted lexicographically by the rules of this new language. Derive the order of letters in this language.
+ *
+ * Example 1:
+ *
+ * Input:
+ * [
+ *   "wrt",
+ *   "wrf",
+ *   "er",
+ *   "ett",
+ *   "rftt"
+ * ]
+ *
+ * Output: "wertf"
+ * Example 2:
+ *
+ * Input:
+ * [
+ *   "z",
+ *   "x"
+ * ]
+ *
+ * Output: "zx"
+ * Example 3:
+ *
+ * Input:
+ * [
+ *   "z",
+ *   "x",
+ *   "z"
+ * ]
+ *
+ * Output: ""
+ *
+ * Explanation: The order is invalid, so return "".
+ * Note:
+ *
+ * You may assume all letters are in lowercase.
+ * You may assume that if a is a prefix of b, then a must appear before b in the given dictionary.
+ * If the order is invalid, return an empty string.
+ * There may be multiple valid order of letters, return any one of them is fine.
  */
 public class AlienOrder {
+    public static void main(String[] args) {
+        AlienOrder a =new AlienOrder();
+        a.alienOrder(new String[]{"wrt","wrf","er","ett","rftt"});
+    }
     public String alienOrder(String[] words) {
         Map<Character, Set<Character>> graph = new HashMap();
         int inDegree[] = new int[26];
@@ -20,9 +68,11 @@ public class AlienOrder {
                 q.offer(ch);
             }
         }
+        //[0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,1,0,0,0]
+        //[0,0,0,0,1,1,
         StringBuilder sb = new StringBuilder();
         while( !q.isEmpty() ){
-            Character ch = q.poll();
+            Character ch = q.poll(  );
 
             Set<Character> neighbours = graph.get(ch);
             for( Character n : neighbours ){
